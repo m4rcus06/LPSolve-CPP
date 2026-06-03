fd76f7505c0494463d7b162452b6eb2f4989f0ac
Hao <94845545+m4rcus06@users.noreply.github.com>
2026-06-03 20:45:44 +0700

    Remove unused function
    Removed unused matrix column operations and identity matrix function.

--- src/include/matrix.h
+++ src/include/matrix.h
@@ -56,36 +56,12 @@ public:
         for (int j = 0; j < cols; ++j) (*this)(r, j) *= k;
     }
 
-    /** 
-     * @brief Multiply c-th column by k (0-based index)
-     * */
-    void multiplyCol(int c, T k) {
-        assert(c >= 0 && c < cols);
-        for (int i = 0; i < rows; ++i) (*this)(i, c) *= k;
-    }
-
     /**
      * @brief Row(to) = Row(to) + k * Row(from) 
      * */
     void addRow(int from, int to, T k) {
         assert(from >= 0 && from < rows && to >= 0 && to < rows);
         for (int j = 0; j < cols; ++j) (*this)(to, j) += k * (*this)(from, j);
-    }
-
-    /**
-     * @brief Col(to) = Col(to) + k * Col(from) 
-     * */
-    void addCol(int from, int to, T k) {
-        assert(from >= 0 && from < cols && to >= 0 && to < cols);
-        for (int i = 0; i < rows; ++i) (*this)(i, to) += k * (*this)(i, from);
-    }
-
-    /**
-     * @brief Swap two row r1 an r2
-     * */
-    void swapRow(int r1, int r2) {
-        assert(r1 >= 0 && r1 < rows && r2 >= 0 && r2 < rows);
-        for (int j = 0; j < cols; ++j) std::swap((*this)(r1, j), (*this)(r2, j));
     }
 
     /**
@@ -101,31 +77,6 @@ public:
         while ((int) data.back().size() < this->colSize()) data.back().push_back(static_cast<T>(0));
         if (this->rowSize() == 0) this->setCol(data.back().size());
         this->setRow(this->rowSize() + 1);
-    }
-
-    /**
-     * @brief add one column to this matrix
-     * */
-    void expandColumn(const std::vector<T>& v) {
-        if (this->empty()) {
-            this->setRow((int) v.size());
-            data.assign(this->rowSize(), std::vector<T>());
-        }
-
-        assert((int) v.size() <= this->rowSize());
-        for (int i = 0; i < this->rowSize(); ++i) {
-            if (i < (int) v.size()) data[i].push_back(v[i]);
-            else data[i].push_back(static_cast<T>(0));
-        }
-        if (this->colSize() == 0) this->setRow((int) v.size());
-
-        this->setCol(this->colSize() + 1);
-    }
-    
-    static Matrix<T> Identity(int n) {
-        Matrix<T> result(n, n);
-        for (int i = 0; i < n; ++i) result(i, i) = static_cast<T>(1);
-        return result;
     }
 
     static Matrix<T> toMatrix(const std::vector<std::vector<T>>& raw_data) {
